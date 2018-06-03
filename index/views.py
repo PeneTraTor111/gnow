762b07dfda49a4c103885549718e5996618fd07c
PeneTraTor111 <275701017@qq.com>
2018-06-03 22:59:23 +0800

    content
    tooltip cannot work.
    There is someting wrong with popper.js.

--- index/views.py
+++ index/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def result(request):
     return render(request, "result.html")
+
+def content(request):
+    introduction = "《塞尔达传说》系列首部登陆任天堂Switch主机的作品，本作也是系列第一款开放世界作品，玩家可以脱出此前的各种限制，在广阔的世界中自由探索。 游戏故事依然是林克拯救世界的故事，海拉尔大陆再次被加农侵袭，林克从长眠中醒来，在冒险中获得武器、能力，最后去挑战加农。但你也可以直接去干掉加农，本作的自由度是系列前所未有的。 在《塞尔达传说 旷野之息》中，林克从一开始就获得了所有道具能力，并且可以和世界中的各种要素进行互动。大自然的风火雷电都可以被利用，让林克的冒险有层出不穷的玩法。 本作中文版将于2018年2月1日推出，中文版标题确认为《塞尔达传说：旷野之息》。"
+    return render(request, "content.html", {'data': introduction})
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('result/', views.result),
+    path('result/content/', views.content),
 ]
